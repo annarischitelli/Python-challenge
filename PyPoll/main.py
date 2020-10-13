6d4308f93c64f82e5d6d8863fb50a57b5ad3c03b
annarischitelli <annarischitelli94@gmail.com>
2020-10-12 20:50:06 -0700

    Python HW attempt

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,16 +5,15 @@ import csv
 election_data = os.path.join("PyPoll", "Resources", "election_data.csv")
 
 # Set variable value
-vote_count = 0
+vote_count = []
 total_votes = 0
 candidates = []
 individual_candidate = []
 percent_total_votes = []
 
-# Read csv and add to sscript
+# Read csv and add to script
 with open (election_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
-
     # Header row
     csv_header = next(csvreader)
 
@@ -22,33 +21,48 @@ with open (election_data) as csvfile:
     for row in csvreader:
         # Add each vote (row) to total votes
         total_votes += 1
-        # Identify candidate in row + add to list
-        if(row[2] not in candidates):
-            candidates.append(row[2])
-            vote_count.append(0)
-        # Add vote to candidate list
-        candidate_votes = candidates.index(row[2])
-        total_votes[candidate_votes] += 1
+        # Identify candidate in row + add to list (if found)
+        candidates = (row[2])
+        if candidates in individual_candidate:
+            candidate_index = individual_candidate.index (candidates)
+            vote_count[candidate_index] = vote_count[candidate_index] + 1
+        else:
+            # if candidate not found
+            individual_candidate.append(candidates)
+            vote_count.append(1)
+
+# Test list so far
+print(f"total_votes {total_votes}")
+print(f"For candidate: {individual_candidate}")
+print(f"Index: {individual_candidate.index(candidates)}")
+
+        
+#         if(row[2] not in candidates):
+#             candidates.append(row[2])
+#             vote_count.append(0)
+#         # Add vote to candidate list
+#         candidate_votes = candidates.index(row[2])
+#         total_votes[candidate_votes] += 1
       
 
-    # Create list of individual candidates
-    for i in set(candidates):
-        individual_candidate.append(i)
-        votes_per_candidate = candidates.count(i)
-        vote_count.append(votes_per_candidate)
-        percent_per_candidate = (votes_per_candidate/count)*100
-        percent_total_votes.append(percent_per_candidate)
+#     # Create list of individual candidates
+#     for i in set(candidates):
+#         individual_candidate.append(i)
+#         votes_per_candidate = candidates.count(i)
+#         vote_count.append(votes_per_candidate)
+#         percent_per_candidate = (votes_per_candidate/count)*100
+#         percent_total_votes.append(percent_per_candidate)
 
-    winner_count = max(vote_count)
-    winner = individual_candidate[vote_count.index(winner_count)]
+#     winner_count = max(vote_count)
+#     winner = individual_candidate[vote_count.index(winner_count)]
 
-# Show all information
-print("Election Results")
-print("----------------")
-print(f"Total Votes: {str(vote_count)}")
-print("----------------")
-for count in range (len(candidates)):
-    print(f"{candidate(count)}: {percent_per_candidate[count]}% ({total_votes[count]})")
-print("----------------")
-print("Winner: {winner}")
-print("----------------")
+# # Show all information
+# print("Election Results")
+# print("----------------")
+# print(f"Total Votes: {str(vote_count)}")
+# print("----------------")
+# for count in range (len(candidates)):
+#     print(f"{candidate(count)}: {percent_per_candidate[count]}% ({total_votes[count]})")
+# print("----------------")
+# print("Winner: {winner}")
+# print("----------------")
